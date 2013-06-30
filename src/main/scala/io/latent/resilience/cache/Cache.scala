431bafde7ea82647cbdeca1b0e4f6b72e28c5c0f
ppat <peter@latent.io>
2013-06-30 15:32:44 -0400

    cache improvements

--- src/main/scala/io/latent/resilience/cache/Cache.scala
+++ src/main/scala/io/latent/resilience/cache/Cache.scala
@@ -1,9 +1,12 @@
 package io.latent.resilience.cache
 
 trait Cache[K, V] {
+  /** Add to cache */
   def put(key: K, value: V)
 
+  /** Get if exists */
   def get(key: K): Option[V]
 
+  /** Will automatically load values when not present using the cacheLoader */
   def get(key: K, cacheLoader: => V): Option[V]
 }
--- src/main/scala/io/latent/resilience/cache/SimpleCache.scala
+++ src/main/scala/io/latent/resilience/cache/SimpleCache.scala
@@ -13,30 +13,35 @@ import com.google.common.base.Optional
 class SimpleCache[K, V](config: CacheConfig) extends Cache[K, V] {
   def this() = this(new CacheConfig())
 
-  // when a cacheLoader is not provided Google CacheBuilder types the cache as [Object, Object]
+  // when a cacheLoader is not provided, Google CacheBuilder types the cache as [AnyRef, AnyRef]
+  // which is unfortunate and leads to the mess that creates the need for use of asInstanceOf within
+  // this whole class. While the Guava CacheBuilder provides an excellent implementation of a local
+  // caching system, it's API is incredibly inelegant. Since it does not allow storage of null within
+  // the cache that forces us to use Guava's version of Option(al) as scala.Option cannot be stored
+  // within a purely java data structure without consequences. But we can hide all that monstrosity
+  // and still leverage the power of the Guava Cache within the SimpleCache by way of encapsulation.
   private val cache: GCache[AnyRef, AnyRef] = create()
 
   def put(key: K,
           value: V): Unit = {
-    if (value != null)
-      cache.put(key.asInstanceOf[AnyRef], value.asInstanceOf[AnyRef])
+    cache.put(key.asInstanceOf[AnyRef], Optional.fromNullable(value).asInstanceOf[AnyRef])
   }
 
-  /** Get if exists */
   def get(key: K): Option[V] = {
     val value = cache.getIfPresent(key)
-    if (value == null) None else Some(value.asInstanceOf[V])
+    guavaOptionalToScalaOption(value)
   }
 
-  /** Will automatically load values when not present */
   def get(key: K,
           cacheLoader: => V): Option[V] = {
-    val value = cache.get(key.asInstanceOf[AnyRef], toCallable(cacheLoader)).asInstanceOf[Optional[V]]
+    val value = cache.get(key.asInstanceOf[AnyRef], toCallable(cacheLoader))
     guavaOptionalToScalaOption(value)
   }
 
 
-  private def guavaOptionalToScalaOption(optionalValue: Optional[V]): Option[V] = {
+  private def guavaOptionalToScalaOption(value: AnyRef): Option[V] = {
+    if (value == null) return None
+    val optionalValue = value.asInstanceOf[Optional[V]]
     if (optionalValue.isPresent) Some(optionalValue.get()) else None
   }
 
@@ -48,15 +53,23 @@ class SimpleCache[K, V](config: CacheConfig) extends Cache[K, V] {
     }
   }
 
-  private def create(): GCache[Object, Object] = {
-    CacheBuilder.newBuilder()
-                .concurrencyLevel(config.concurrencyLevel)
-                .maximumSize(config.maxSize)
-                .expireAfterWrite(config.expiration, TimeUnit.MILLISECONDS)
-                .build()
+  private def create(): GCache[AnyRef, AnyRef] = {
+    var builder = CacheBuilder.newBuilder()
+                              .concurrencyLevel(config.concurrencyLevel)
+                              .maximumSize(config.maxSize)
+                              .expireAfterWrite(config.expiration, TimeUnit.MILLISECONDS)
+
+    if (config.softValues) builder = builder.softValues()
+    if (config.weakValues) builder = builder.weakValues()
+    if (config.weakKeys) builder = builder.weakKeys()
+
+    builder.build()
   }
 }
 
 case class CacheConfig(maxSize: Int = 100,
                        expiration: Int = 24 * 60 * 60 * 1000,
-                       concurrencyLevel: Int = 3)
+                       concurrencyLevel: Int = 4,
+                       softValues: Boolean = false,
+                       weakValues: Boolean = false,
+                       weakKeys: Boolean = false)
