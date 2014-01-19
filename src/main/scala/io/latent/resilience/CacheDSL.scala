d9301849c6caf711b8b4dc7109844bd14edb3e20
ppat <peter@latent.io>
2014-01-19 15:47:24 -0500

    1) remove Hystrix from caching DSL as there was no benefit in wrapping cache loading operations with Hystrix once a Future of result is stored in the Cache.For individual use cases where this might be benefical, you can still wrap your cache loading operation with Hystrix yourself. 2) add implicit block() method to Futures' returned by Cache DSL so that non-asynchronous code easily work with them

--- src/main/scala/io/latent/resilience/CacheDSL.scala
+++ src/main/scala/io/latent/resilience/CacheDSL.scala
@@ -1,10 +1,10 @@
 package io.latent.resilience
 
 import io.latent.resilience.cache.Cache
-import io.latent.resilience.HystrixDSL._
-import scala.concurrent.{ExecutionContext, Future, Promise}
+import scala.concurrent.{Await, ExecutionContext, Future, Promise}
 import ExecutionContext.Implicits.global
-
+import scala.util.{Failure, Success}
+import scala.concurrent.duration._
 
 /**
  * A DSL for building local, in-memory caches intended for use with queries returning small data sets.
@@ -15,12 +15,12 @@ object CacheDSL {
   /** get from cache if exists, otherwise load cache with query and return result. Falling back to None */
   def cached[K, V](key: K)
                   (op: => V)
-                  (implicit cache: Cache[K, V], config: HystrixConfig): Future[V] = {
+                  (implicit cache: Cache[K, V]): Future[V] = {
     cache.get(key) match {
       case None => {
         val promise = Promise[V]()
         cache.put(key, promise.future)
-        val future: Future[V] = buildFuture(op)
+        val future = Future[V](op)
         future.onComplete {
           value => {
             promise.complete(value)
@@ -33,8 +33,20 @@ object CacheDSL {
     }
   }
 
-  private def buildFuture[V](op: => V)(implicit config: HystrixConfig): Future[V] = {
-    def action(): V = gracefully(op).degrade()
-    Future[V](action)
+  implicit def blockingFuture[V](future: Future[V]) = new Object {
+    /** Block for future to complete (for non-asynchronous usage). Fallback will be used if future does not complete in time or if
+      * an error occurred and fallbackOnError is true */
+    def block(timeout: Duration = 1 second, fallback: => Option[V] = None, fallbackOnError: Boolean = false): Option[V] = {
+      Await.ready(future, timeout).value match {
+        case Some(Success(value)) => {
+          value match {
+            case null => None
+            case something => Some(something.asInstanceOf[V])
+          }
+        }
+        case Some(Failure(e)) if (!fallbackOnError) => throw e
+        case _ => fallback
+      }
+    }
   }
 }
